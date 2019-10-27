b0e1497aaf19781ae3730ae525fde95fae72465b
marcellatwin <56375636+marcellatwin@users.noreply.github.com>
2019-10-26 17:05:50 -0700

    Updated jc_main

---
+++ jc_main.cpp
@@ -0,0 +1,31 @@
+/*****************************************************************************/
+/* 
+ * jc_main.cpp
+ * By Jonathan Crossley
+ * Version 0.1 - 24 Oct 2019
+ *         
+ */
+
+// Socket class object library
+#include "jc_CAN_socket.h"
+
+// Standard libraries
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	// Set up socket and CAN frame
+	CAN_socket sock;
+	struct can_frame frame;
+
+	for (int i = 0; i < 10; i++)
+	{
+		// Get a frame to test
+		frame = sock.read_frame(frame);
+
+		// Display
+		cout << frame.can_id << " " << frame.data << endl;
+	}
+}
